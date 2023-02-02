9e4530a8d6a266ea7c7c33858c20dceead7300ec
You Kim Chhay <41745241+YouKimChhay@users.noreply.github.com>
2023-02-02 14:55:00 -0600

    w3d4 express q1

---
+++ q1/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  let name = req.query.name;
+  if (!name) {
+    name = "person";
+  }
+  let age = req.query.age;
+  if (!age) {
+    age = 0;
+  }
+  res.send(`Welcome ${name}, ${age} years old.`);
+});
+
+app.listen(3000);
